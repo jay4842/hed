6c43914c1595815705a689dc68b4ef68bb0764d1
Jimmy Johnson <jay4842@gmail.com>
2018-04-18 14:38:09 -0500

    Update solve.py

--- examples/hed/solve.py
+++ examples/hed/solve.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import sys
 caffe_root = '../../' 
-sys.path.insert(0,"/home/ubuntu/src/caffe_python_2/python/")
+sys.path.insert(0,caffe_root)
 import caffe
 
 # make a bilinear interpolation kernel
